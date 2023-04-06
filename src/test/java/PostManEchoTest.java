dc8f8eaffb5c9d36834133f88ab8a9d932428b46
Nikita Samoilov <nikisamoylov@gmail.com>
2023-04-06 13:06:53 +0400

    fixed test file

--- src/test/java/PostManEchoTest.java
+++ src/test/java/PostManEchoTest.java
@@ -1,24 +1,29 @@
-import groovy.transform.ASTTest;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 import static io.restassured.RestAssured.given;
 
 
 public class PostManEchoTest {
 
-@Test
+    @Test
     void ShouldReturnSetData() {
-    // Given - When - Then
+        // Given - When - Then
 // Предусловия
-    given()
-            .baseUri("https://postman-echo.com")
-            .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-            .when()
-            .post("/post")
+                .when()
+                .post("/post")
 // Проверки
-            .then()
-            .statusCode(200)
-            .body("Hello")
-    ;
-}
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
 }
